8ed9f6e55c9dbfe7122d12a14ee62cba7026a82f
Ginam Kim <125203829+ginam-Kim@users.noreply.github.com>
2024-08-05 15:00:29 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.py
@@ -4,3 +4,8 @@ def solution(num_list):
     else:
         answer = eval(str('*'.join([str(n) for n in num_list])))
     return answer
+
+# math 라이브러리를 이용한 풀이
+from math import prod
+def solution(num_list):
+    return sum(num_list) if len(num_list)>=11 else prod(num_list)
