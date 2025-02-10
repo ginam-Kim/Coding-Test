c4eedf7f092bf5d727a138abce3182175803eb3a
Ginam Kim <125203829+ginam-Kim@users.noreply.github.com>
2025-02-10 18:23:21 +0900

    [level 1] Title: 아픈 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
+++ 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
@@ -1,5 +1,9 @@
 -- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME 
-    from ANIMAL_INS
-    where intake_condition = 'Sick'
-    order by ANIMAL_ID;
+SELECT 
+    ANIMAL_ID, NAME 
+FROM 
+    ANIMAL_INS
+WHERE 
+    intake_condition = 'Sick'
+ORDER BY 
+    ANIMAL_ID;
