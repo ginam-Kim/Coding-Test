f89350dd4b70d0464952a91e52d73565006cb6f3
Ginam Kim <125203829+ginam-Kim@users.noreply.github.com>
2025-04-05 01:22:45 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,35 +1,28 @@
 -- 코드를 작성해주세요
 # WITH RECURSIVE
 
-WITH RECURSIVE ECOLI_DATA_GEN AS(
+WITH RECURSIVE E_GEN AS(
     SELECT
-        ID,
-        PARENT_ID,
-        1 AS GEN
+        ID, PARENT_ID, 1 AS GEN
     FROM
-        ECOLI_DATA 
+        ECOLI_DATA
     WHERE
         PARENT_ID IS NULL
     
     UNION ALL
     
     SELECT
-        E.ID,
-        E.PARENT_ID,
-        GEN+1 AS GEN
+        E.ID, E.PARENT_ID, GEN+1 AS GEN 
     FROM
-        ECOLI_DATA_GEN 
-    JOIN
-        ECOLI_DATA AS E
-    ON
-        ECOLI_DATA_GEN.ID = E.PARENT_ID
+        E_GEN JOIN ECOLI_DATA E
+        ON E_GEN.ID=E.PARENT_ID
 )
 
 SELECT
     ID
 FROM
-    ECOLI_DATA_GEN
+    E_GEN
 WHERE
-    GEN = 3
-ORDER BY 
+    GEN=3
+ORDER BY
     ID ASC;
